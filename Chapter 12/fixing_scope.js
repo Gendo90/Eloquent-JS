e63e6e9eb0cbeb8a4f63d997a2fbd4bafe9a7130
Patrick Gendotti <patrick.gendotti@gmail.com>
2019-09-04 11:37:59 -0700

    feat: Add solution to "Fixing Scope" problem from Eloquent JS
    Note: It was tricky to get the outer scope, needed to use
    Object.getPrototypeOf, and to check if the variable is in the local
    scope first, by "inheriting" Object.prototype's "hasOwnProperty"
    function to use with the inner scope object

---
+++ Chapter 12/fixing_scope.js
@@ -0,0 +1,31 @@
+// let egg = require('12_language')
+
+specialForms.set = (args, scope) => {
+  // Your code here.
+  if (args.length != 2 || args[0].type != "word") {
+    throw new SyntaxError("Incorrect use of set");
+  }
+  let value = evaluate(args[1], scope);
+  let outerScope = Object.getPrototypeOf(scope)
+  let var_name = args[0].name
+
+  if(Object.prototype.hasOwnProperty.call(scope, var_name)) {
+      scope[var_name] = value;
+  }
+  else if(Object.prototype.hasOwnProperty.call(outerScope, var_name)) {
+      outerScope[var_name] = value;
+  }
+  else {
+      throw new ReferenceError(`${var_name} not previously declared`)
+  }
+};
+
+run(`
+do(define(x, 4),
+   define(setx, fun(val, set(x, val))),
+   setx(50),
+   print(x))
+`);
+// → 50
+run(`set(quux, true)`);
+// → Some kind of ReferenceError
